fc41ec50dd68f466f3e4379406e0857e8189669f
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-13 02:47:56 -0500

    Include a function to format the spots and pass the function as prop

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,11 +8,23 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
-
+  const formatSpots = function(props) {
+    let spotsStatus = '';
+    if (props.spots === 0) {
+      spotsStatus = 'no spots remaining';
+    }
+    if (props.spots === 1) {
+      spotsStatus = '1 spot remaining';
+    }
+    if (props.spots === 2){
+      spotsStatus = '2 spots remaining';
+    }
+    return spotsStatus;
+  }
   return (
     <li className={dayListClass} onClick = {() => {props.setDay(props.name)}}>
-      <h2 >{props.name}</h2> 
-      <h3 >{props.spots} remaining</h3>
+      <h2>{props.name}</h2> 
+      <h3>{formatSpots(props)}</h3>
     </li>
   );
 }
