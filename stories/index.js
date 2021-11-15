c8b9302e19dfe3f7efb93b09bee0d828a3eb8448
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-15 02:52:31 -0500

    Update the 'clickable' story of InterviewerListItem component

--- stories/index.js
+++ stories/index.js
@@ -96,13 +96,11 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
       ))
       .add("Clickable", () => (
         <InterviewerListItem
-          id={interviewer.id}
           name={interviewer.name}
           avatar={interviewer.avatar}
-          setInterviewer={action("setInterviewer")}
+          setInterviewer={() => action("setInterviewer")(interviewer.id)}
         />
       ));
-
       const interviewers = [
         { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
         { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
