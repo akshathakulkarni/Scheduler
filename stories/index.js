0ecdc144424479752733100655e3d75137770023
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-15 15:37:02 -0500

    Update the InterviewerList storied to use updated prop names

--- stories/index.js
+++ stories/index.js
@@ -121,12 +121,12 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
         .add("Selected", () => (
           <InterviewerList
             interviewers={interviewers}
-            interviewer={3}
+            value={3}
           />
         ))
         .add("Clickable", () => (
           <InterviewerList
             interviewers={interviewers}
-            setInterviewer={action("setInterviewer")}
+            onChange={action("setInterviewer")}
           />
         ));
