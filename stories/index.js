9cbdb6ef456482c7a0aebbbccc4e25bfd44dbac9
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-16 01:34:28 -0500

    Add new stories to test Empty component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)  //Initiates Storybook and registers our Button component
   .addParameters({
@@ -137,4 +139,6 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
           backgrounds: [{ name: "white", value: "#fff", default: true }]
         })
         .add("Appointment", () => <Appointment />)
-        .add("Appointment with Time", () => <Appointment time="12pm" />);
+        .add("Appointment with Time", () => <Appointment time="12pm" />)
+        .add("Header", () => <Header time="12pm" />)
+        .add("Empty", () => <Empty onAdd={action("onAdd")} />)
