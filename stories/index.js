b0193e6e9a35cbfde83f62e21e004b73e7b00d60
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-16 02:25:02 -0500

    Add story to test the Error component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)  //Initiates Storybook and registers our Button component
   .addParameters({
@@ -150,5 +151,6 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
         .add("Confirm", () => <Confirm message={"Delete the appointment?"} onConfirm={action("onConfirm")}
         onCancel={action("onCancel")}/>)
         .add("Status", () => <Status message={"Deleting"}/>)
+        .add("Error", () => <Error message={"Could not delete appointment."} onClose={action("onClose")}/>)
 
       
