e21c39c57960975209ff0002bc35a0df33f02452
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-17 01:10:46 -0500

    Build the Form component and set a default state to student and interviewer, update the <input> tag to use state values

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => {setStudent(event.target.value)}}
+          />
+        </form>
+        <InterviewerList 
+        /* your code goes here */
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger /* your code goes here *}>Cancel</Button>
+          <Button confirm /* your code goes here */>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
