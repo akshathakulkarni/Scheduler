7f098629e4bb56695aabf96aba6f56dc62ec3dd6
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-17 02:58:56 -0500

    Assign necessary props to the Cancel and Save buttons

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,10 +5,16 @@ import Button from "components/Button";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = () => {
+    return [setStudent(''), setInterviewer(null)];
+  }
+  const cancel = () => {
+    return props.onCancel(reset());
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -26,8 +32,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger /* your code goes here *}>Cancel</Button>
-          <Button confirm /* your code goes here */>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
