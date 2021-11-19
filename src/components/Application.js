e3e024dcda9a6316d92acc0666ec4c7f61038e62
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-19 16:15:26 -0500

    Make a GET request to /api/days using axios to fetch data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -62,7 +46,14 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+useEffect(() => {
+  axios.get('/api/days').then(response => {
+    console.log('response from axios = ', response.data);
+    setDays([...response.data]);
+  }, [])
+})
   const appointmentListArray = Object.values(appointments);
   const appointmentList = appointmentListArray.map(appointment => {
     return (
@@ -86,8 +77,8 @@ export default function Application(props) {
         <DayList
           key={days.id}
           days={days}
-          value={day}
-          onChange={setDay}/>
+          value={days}
+          onChange={setDays}/>
       </nav>
       <img
         className="sidebar__lhl sidebar--centered"
