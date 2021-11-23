fcd1891bc81147abc1955814c8c04f0e55bc9150
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-23 00:09:15 -0500

    Add a back() action to useVisualMode to return to the previous mode when called

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,8 +2,23 @@ import React, {useState} from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  const transition = (mode) => {
-    setMode(mode);
+  const [history, setHistory] = useState([initial]);
+  console.log('history = ', history);
+  const transition = (newMode) => {
+    setMode(newMode);
+    history.push(newMode);
+    console.log('history from transition = ', history);
   }
-  return { mode, transition };
+  const back = (newMode) => {
+    if (mode === initial) {
+      setMode(initial);
+    } else {
+      history.pop();
+      console.log('history from back = ', history);
+      setMode(history[history.length - 1]);
+    }
+    
+    
+  }
+  return { mode, transition, back };
 }
