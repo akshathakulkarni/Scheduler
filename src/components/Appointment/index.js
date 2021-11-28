c325e05cc5196760c977aa6bdce69be5e99bc114
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-28 13:13:17 -0500

    Handle errors for the put and delete routes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,13 +6,14 @@ import Empty from './Empty';
 import Form from './Form';
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 import useVisualMode from 'hooks/useVisualMode';
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
-const DELETE = "DELETE";
+const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
 const ERROR_SAVE = "ERROR_SAVE";
@@ -31,13 +32,14 @@ export default function Appointment(props) {
     };
     transition(SAVING);
     props.bookInterview(props.id, interview)
-    .then(() => transition(SHOW));
+    .then(() => transition(SHOW))
+    .catch((error) => transition(ERROR_SAVE, true));
   }
   function deleteInterview() {
-    
-    transition(DELETE);
+    transition(DELETING, true);
     props.cancelInterview(props.id)
     .then(() => transition(EMPTY))
+    .catch((error) => transition(ERROR_DELETE, true));
   }
   function updateInterview() {
     transition(EDIT);
@@ -62,7 +64,9 @@ export default function Appointment(props) {
       {mode === SAVING && <Status message={"Saving.."}/>}
       {mode === CONFIRM && <Confirm message={"Are you sure you want to delete this interview"
 } onCancel={() => back()} onConfirm={deleteInterview}/>}
-      {mode === DELETE && <Status message={"Deleting.."}/>}
+      {mode === DELETING && <Status message={"Deleting.."}/>}
+      {mode === ERROR_SAVE && <Error message={"Sorry, we could not create the appointment"} onClose={() => back()}/>}
+      {mode === ERROR_DELETE && <Error message={"Could not delete the appointment"} onClose={() => back()} />}
       {mode === EDIT && 
         <Form 
           student={props.interview.student}   
