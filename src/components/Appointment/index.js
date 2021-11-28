fcc3e376aff06a3e270d8e8dbf18406832112108
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-28 02:28:12 -0500

    Add a new deleteInterview function to delete the interview based on id and update the component to show respective child components based on modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,12 +5,15 @@ import Show from './Show';
 import Empty from './Empty';
 import Form from './Form';
 import Status from './Status';
+import Confirm from './Confirm';
 import useVisualMode from 'hooks/useVisualMode';
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETE = "DELETE";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
   console.log('Appointment props received = ', props);
@@ -27,6 +30,13 @@ export default function Appointment(props) {
     props.bookInterview(props.id, interview);
     transition(SHOW);
   }
+  function deleteInterview() {
+    transition(CONFIRM);
+    transition(DELETE);
+    props.cancelInterview(props.id);
+    transition(EMPTY);
+  }
+  
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -38,10 +48,13 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={deleteInterview}
         />
       )}
       {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back()} onSave={save} />}
-      {mode === SAVING && <Status message={"Saving your data"}/>}
+      {mode === SAVING && <Status message={"Saving.."}/>}
+      {mode === CONFIRM && <Confirm />}
+      {mode === DELETE && <Status message={"Deleting.."}/>}
       {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> : <Empty /> }  */}
       {/* {props.time && <h2>Appointment at {props.time}</h2>}
       {!props.time && <h2>No Appointments</h2>} */}
