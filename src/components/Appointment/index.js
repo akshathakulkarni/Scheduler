5a04750e71789077c8cff2bd32850b018e965d38
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-11-28 01:20:43 -0500

    Add a new save function and update the mode to show status component when user clicks on Save after creating a new appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,16 +4,29 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 import Form from './Form';
+import Status from './Status';
 import useVisualMode from 'hooks/useVisualMode';
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
-const CREATE = 'CREATE';
+const CREATE = "CREATE";
+const SAVING = "SAVING";
+
 export default function Appointment(props) {
   console.log('Appointment props received = ', props);
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+  function save(name, interviewer) {
+    console.log('SAvve function = ', name, interviewer);
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview);
+    transition(SHOW);
+  }
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -27,7 +40,8 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         />
       )}
-      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back()}/>}
+      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back()} onSave={save} />}
+      {mode === SAVING && <Status message={"Saving your data"}/>}
       {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> : <Empty /> }  */}
       {/* {props.time && <h2>Appointment at {props.time}</h2>}
       {!props.time && <h2>No Appointments</h2>} */}
