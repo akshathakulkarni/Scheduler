8001703f516e3eedc87b69e45ea7fba317d18fd8
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-12-03 13:54:23 -0500

    Add data-testid with the value student-name-input to the input element and a validate function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,11 +5,19 @@ import Button from "components/Button";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
   const reset = () => {
     return [setStudent(''), setInterviewer(null)];
   }
   const cancel = () => {
     return props.onCancel(reset());
+  }
+  const validate = () => {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    props.onSave(student, interviewer);
   }
   return (
     <main className="appointment__card appointment__card--create">
@@ -22,8 +30,11 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => {setStudent(event.target.value)}}
+            data-testid="student-name-input"
+            
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers} 
           value={interviewer}
@@ -33,7 +44,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
