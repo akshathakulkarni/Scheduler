7a0c20754e6ee875b3b17dfc83584032fd109417
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-12-07 00:12:04 -0500

    Add a data test id prop to the root article element of the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -44,7 +44,7 @@ export default function Appointment(props) {
   }
   
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => {
         transition(CREATE);
@@ -71,6 +71,7 @@ export default function Appointment(props) {
           interviewers={props.interviewers} 
           onCancel={() => back()} 
           onSave={save}/>}
+      
     </article>
   );
 };
